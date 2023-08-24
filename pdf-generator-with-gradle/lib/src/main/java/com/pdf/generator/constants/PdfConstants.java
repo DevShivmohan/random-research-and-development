62297083c3739452b8cb01c5211c0850720631ec
Shivmohan <nshivmohan08@gmail.com>
2023-08-24 14:26:19 +0530

    Added pdf-generator library with gradle

---
+++ pdf-generator-with-gradle/lib/src/main/java/com/pdf/generator/constants/PdfConstants.java
@@ -0,0 +1,8 @@
+package com.pdf.generator.constants;
+
+public class PdfConstants {
+    public static final String ERROR_MESSAGE="Illegal input files or permissions denied";
+    public static final String PDF_EXTENSION=".pdf";
+    public static final String DOUBLE_UNDERSCORE="__";
+    public static final String DOT=".";
+}
